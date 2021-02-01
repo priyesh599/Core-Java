b608acd71483d27802db4a573b2f307c04a73a57
priyesh599 <60017653+priyesh599@users.noreply.github.com>
2021-02-01 12:13:01 +0530

    Add files via upload

---
+++ Assignment 4/Assignment4 Q6.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Assignment4Q6 {
+    public static void main(String[] args) {
+    	
+    }
+    public List<String> convertToUpperCase(List<String> list) {
+    	list.replaceAll(s->s.toUpperCase());
+    	return list;
+    }  
+}
